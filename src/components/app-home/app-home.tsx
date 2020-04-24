7c26a2b17931b33a82d2e894605e499530b20d92
Bahit Hamid <bahit.hamid@gmail.com>
2020-04-24 21:57:35 +0800

    First draft for intro landing page

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -1,29 +1,55 @@
-import { Component, h } from '@stencil/core';
+import { Component, h } from "@stencil/core";
 
 @Component({
-  tag: 'app-home',
-  styleUrl: 'app-home.css'
+  tag: "app-home",
+  styleUrl: "app-home.css",
 })
 export class AppHome {
-
   render() {
     return [
       <ion-header>
         <ion-toolbar color="primary">
-          <ion-title>Home</ion-title>
+          <ion-title>Ramadan 11</ion-title>
         </ion-toolbar>
       </ion-header>,
 
       <ion-content class="ion-padding">
         <p>
-          Welcome to the PWA Toolkit. You can use this starter to build entire
-          apps with web components using Stencil and ionic/core! Check out the
-          README for everything that comes in this starter out of the box and
-          check out our docs on <a href="https://stenciljs.com">stenciljs.com</a> to get started.
+          Welcome to Ramadan 11, a Progressive Web Application (PWA) that helps
+          you in your Ramadan prayer for Terawih and Witir. This PWA focuses on
+          8 raka'at for Terawih and 3 raka'at for Witir.
         </p>
 
-        <ion-button href="/profile/ionic" expand="block">Profile page</ion-button>
-      </ion-content>
+        <p>
+          This PWA guides you step by step for surah to read for all the 8
+          raka'at for Terawih and 3 raka'at for Witir.
+        </p>
+
+        <h1>Summary</h1>
+        <h2>Terawih</h2>
+        <p>Number below indicate raka'at</p>
+        <ol>
+          <li>Al Fatihah + al-Takathur</li>
+          <li>Al Fatihah + al-Iklas</li>
+          <li>Al Fatihah + al-Asr</li>
+          <li>Al Fatihah + al-Iklas</li>
+          <li>Al Fatihah + al-Humazah</li>
+          <li>Al Fatihah + al-Iklas</li>
+          <li>Al Fatihah + al-Fill</li>
+          <li>Al Fatihah + al-Iklas</li>
+        </ol>
+        <h2>Witir</h2>
+        <p>Number below indicate raka'at</p>
+        <ol>
+          <li>Al Fatihah + al a'la*</li>
+          <li>Al Fatihah + al-Iklas*</li>
+          <li>Al Fatihah + al-Iklas + al Falaq + An nas**</li>
+        </ol>
+        <p>Note: *two raka'at together. **one raka'at</p>
+        <ion-button href="/profile/ionic" expand="block">
+          Profile page
+        </ion-button>
+      </ion-content>,
     ];
   }
 }
